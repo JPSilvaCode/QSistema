82660d7ea21d9f228e646a233f9ab29889b09885
João Paulo da Silva <jpsilvafla@hotmail.com>
2018-02-06 14:42:14 -0200

    Adicionar arquivos de projeto.

---
+++ QSistema/Context/LojaContext.cs
@@ -0,0 +1,14 @@
+﻿using QSistema.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QSistema.Context
+{
+    public class LojaContext : DbContext
+    {       
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
